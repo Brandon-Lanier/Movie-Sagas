bdcb4f3317ddcfec8708d026eb63fdf9fb52e697
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-03 23:09:37 -0600

    Added SQL text to genre router to handle getting all the genres for a selcted movie

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
   res.sendStatus(500)
 });
 
+router.get('/:id', (req, res) => {
+  const Id = req.params.id
+  console.log('movie id is', id);
+  const qryTxt = `
+  SELECT ARRAY_AGG(genres.name) FROM genres
+  JOIN movies_genres ON movies_genres.genre_id = genres.id
+  JOIN movies ON movies_genres.movie_id = movies.id
+  WHERE movies.id = $1;
+  `;
+  pool.query(qryTxt, [id])
+  .then(result => {
+    res.send(result.rows)
+  }).catch(err => {
+    res.sendStatus(500)
+  })
+})
+
 module.exports = router;
